8da356051f52e33bdae7b9184d0f19fe9993224c
MihMarMar <genderlik@gmail.com>
2020-06-20 22:49:36 +0200

    [manarm-gui] disable buttons when axis end is reached, renaming of variables

--- src/gui/move/axis_frame.py
+++ src/gui/move/axis_frame.py
@@ -2,52 +2,65 @@ import tkinter as tk
 
 
 class AxisFrame(tk.Frame):
-    angle = 0
+    _angle = 0
 
     def __init__(self, parent, **kwargs):
-        tk.Frame.__init__(self, parent)
-        self.min_angle = kwargs.get("min_angle", -360)
-        self.max_angle = kwargs.get("max_angle", 360)
-        axis_name = kwargs.get("name", "Axis")
+        self._min_angle = kwargs.pop("min_angle", -360)
+        self._max_angle = kwargs.pop("max_angle", 360)
+        _axis_name = kwargs.pop("axis_name", "Axis")
+        super().__init__(parent, **kwargs)
         # todo add callbacks to button presses as kwargs
 
-        self.label = tk.Label(self, text=axis_name)
-        self.label.pack(side=tk.LEFT, padx=(8, 8))
+        self._label = tk.Label(self, text=_axis_name)
+        self._label.pack(side=tk.LEFT, padx=(8, 8))
 
-        self.left_button = tk.Button(self)
-        left_btn_img = tk.PhotoImage(file="move/img/left-arrow-icon.png")
-        self.left_button.config(image=left_btn_img)
-        self.left_button.image = left_btn_img
-        self.left_button.bind(sequence="<Button-1>", func=self.left_btn_action)
-        self.left_button.pack(side=tk.LEFT)
+        self._left_button = tk.Button(self, command=self._left_btn_action)
+        _left_btn_img = tk.PhotoImage(file="move/img/left-arrow-icon.png")
+        self._left_button.config(image=_left_btn_img)
+        self._left_button.image = _left_btn_img
+        self._left_button.pack(side=tk.LEFT)
 
-        self.angle_slider_indicator = tk.Scale(self, from_=-720, to=720, orient=tk.HORIZONTAL)
-        self.angle_slider_indicator.config(state=tk.DISABLED, takefocus=0)
-        self.angle_slider_indicator.pack(side=tk.LEFT)
+        self._angle_slider_indicator = tk.Scale(self, from_=self._min_angle, to=self._max_angle, orient=tk.HORIZONTAL)
+        self._angle_slider_indicator.config(state=tk.DISABLED, takefocus=0)
+        self._angle_slider_indicator.pack(side=tk.LEFT)
 
-        self.right_button = tk.Button(self)
-        right_btn_img = tk.PhotoImage(file="move/img/right-arrow-icon.png")
-        self.right_button.config(image=right_btn_img)
-        self.right_button.image = right_btn_img
-        self.right_button.bind(sequence="<Button-1>", func=self.right_btn_action)
-        self.right_button.pack(side=tk.LEFT)
+        self._right_button = tk.Button(self, command=self._right_btn_action)
+        _right_btn_img = tk.PhotoImage(file="move/img/right-arrow-icon.png")
+        self._right_button.config(image=_right_btn_img)
+        self._right_button.image = _right_btn_img
+        self._right_button.pack(side=tk.LEFT)
 
     # todo add hold functionality to the buttons
-    def left_btn_action(self, event):
+    def _left_btn_action(self):
+        self.set_angle(self._angle - 10)
         print("Left btn clicked")
         # todo
 
-    def right_btn_action(self, event):
+    def _right_btn_action(self):
+        self.set_angle(self._angle + 10)
         print("Right btn clicked")
         # todo
 
     def set_angle(self, angle):
-        if not self.min_angle <= angle <= self.max_angle:
+        if not self._min_angle <= angle <= self._max_angle:
             raise ValueError(f"Given angle {angle}"
-                             f" is not between {self.min_angle} and {self.max_angle}!")
-        self.angle_slider_indicator.config(state=tk.ACTIVE)
-        self.angle_slider_indicator.set(angle)
-        self.angle_slider_indicator.config(state=tk.DISABLED)
+                             f" is not between {self._min_angle} and {self._max_angle}!")
+
+        self._angle = angle
+        self._angle_slider_indicator.config(state=tk.NORMAL)
+        self._angle_slider_indicator.set(angle)
+        self._angle_slider_indicator.config(state=tk.DISABLED)
+
+        #  handle buttons enabled/disabled
+        if self._angle == self._max_angle:
+            self._right_button.config(state=tk.DISABLED)
+        else:
+            self._right_button.config(state=tk.NORMAL)
+
+        if self._angle == self._min_angle:
+            self._left_button.config(state=tk.DISABLED)
+        else:
+            self._left_button.config(state=tk.NORMAL)
 
     def get_angle(self):
-        return self.angle_slider_indicator.get()
+        return self._angle_slider_indicator.get()
